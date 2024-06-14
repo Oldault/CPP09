c11fab0e672cdeeff68214e035ffffc0e8eeba46
Simon Mac <MonkeyManDeveloper@protonmail.com>
2024-06-14 10:26:03 +0200

    mod init

---
+++ ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oldault <oldault@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 10:22:28 by oldault           #+#    #+#             */
+/*   Updated: 2024/06/14 10:25:40 by oldault          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __BITCOINEXCHANGE_HPP__
+#define __BITCOINEXCHANGE_HPP__
+
+#include <iostream>
+#include <string>
+
+#endif // __BITCOINEXCHANGE_HPP__
