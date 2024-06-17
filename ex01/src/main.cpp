78ddfb723fd6c0103c07e8407b0347b4cb482354
Simon Mac <MonkeyManDeveloper@protonmail.com>
2024-06-17 20:52:58 +0200

    arg as param

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oldault <oldault@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 14:17:35 by oldault           #+#    #+#             */
-/*   Updated: 2024/06/17 17:32:04 by oldault          ###   ########.fr       */
+/*   Updated: 2024/06/17 20:50:24 by oldault          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,23 +34,39 @@ bool isToken(const std::string& s)
   return (s == "+" || s == "-" || s == "*" || s == "/");
 }
 
+void printDemo()
+{
+  std::cout << "\n\t"
+    << BBLU(BOLD(" Please give "))
+    << BBLU(ITAL("`./RPN`"))
+    << BBLU(BOLD(" an argument."))
+    << "\n\n\t" << UNDL(" EXAMPLES: ")
+    << "\n\t" << ITAL(FMAG("./RPN \"3 4 +\""))
+    << "\n\t" << ITAL(FMAG("./RPN \"8 9 * 9 - 9 - 9 - 4 - 1 +\""))
+    << "\n\t" << ITAL(FMAG("./RPN \"3 5 + 7 2 - *\""))
+    << "\n\t" << ITAL(FMAG("./RPN \"1 2 3 4 + * +\""))
+  << std::endl << std::endl;
+}
+
 int main(int ac, char **av)
 {
-  (void)ac;
-  (void)av;
+  if (ac != 2) {
+    printDemo();
+    return - 42;
+  }
   std::stack<int> _stack;
   
-  std::string input = "8 9 * 9 - 9 - 9 - 4 - 1 +";
+  std::string input = av[1];
   std::vector<std::string> x = split(input, ' ');
   
   for (std::vector<std::string>::size_type i = 0; i < x.size(); i++)
   {
     std::string element = x.at(i);
-    std::cout << i << ": " << element << " -> ";
+    std::cout << i << ": " << "\'" << element << "\'" << " -> ";
     if (isToken(element))
     {
       std::cout << "Token" << std::endl;
-      if (_stack.size() != 2)
+      if (_stack.size() < 2)
       {
         std::cerr << "Not enough elements inside stack" << std::endl;
         return -1;
@@ -60,16 +76,12 @@ int main(int ac, char **av)
       int temp2 = _stack.top();
       _stack.pop();
       if (element == "+") {
-        std::cout << "doing " << temp2 << element << temp1 << std::endl;
         _stack.push(temp2 + temp1);
       } else if (element == "-") {
-        std::cout << "doing " << temp2 << element << temp1 << std::endl;
         _stack.push(temp2 - temp1);
       } else if (element == "*") {
-        std::cout << "doing " << temp2 << element << temp1 << std::endl;
         _stack.push(temp2 * temp1);
       } else if (element == "/") {
-        std::cout << "doing " << temp2 << element << temp1 << std::endl;
         _stack.push(temp2 / temp1);
       }
     }
@@ -88,5 +100,5 @@ int main(int ac, char **av)
   std::cout << std::endl;
   
   std::cout << "End of program result = " << _stack.top() << std::endl;
-  return 0;
+  return _stack.top();
 }
